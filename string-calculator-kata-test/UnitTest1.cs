82d7e0c31c91425f674dd2fa3ed529d3c262ce50
Adam Bonarrigo <adam.bonarrigo@myob.com>
2020-03-23 14:36:31 +1100

    First commit

---
+++ string-calculator-kata-test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace string_calculator_kata_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
