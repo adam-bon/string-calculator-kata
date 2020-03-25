d9fb99cf38bafb935f5544c888b5afe1f013a1a4
Adam Bonarrigo <adam.bonarrigo@myob.com>
2020-03-25 15:20:48 +1100

    Wrote first 3 tests and made them pass

---
+++ string-calculator-kata-test/StringCalculatorTests.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+
+namespace string_calculator_kata
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void StringCalculator_inputEmptyString_returnsZero() {
+
+            const int expected = 0;
+            int actual = new StringCalculator().Calculate("");
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void StringCalculator_inputSingleNumberString_returnsNumberAsInt() {
+
+            const int expected = 1;
+            int actual = new StringCalculator().Calculate("1");
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void StringCalculator_inputNumberThreeAsString_returnsTwoAsInt() {
+
+            const int expected = 3;
+            int actual = new StringCalculator().Calculate("3");
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
---
+++ string-calculator-kata/StringCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace string_calculator_kata {
+    public class StringCalculator {
+        public StringCalculator() {
+        }
+
+        public int Calculate(String input) {
+            if (input == "")
+                return 0;
+            else
+                return Int32.Parse(input);
+        }
+    }
+}
