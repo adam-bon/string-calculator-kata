a90d8263d6d198cab67bcf547ca41b84ac4316d5
Michelle <wong.mich.c@gmail.com>
2020-03-25 16:21:52 +1100

    passed test 3 adding two numbers

--- string-calculator-kata/StringCalculator.cs
+++ string-calculator-kata/StringCalculator.cs
@@ -1,14 +1,36 @@
 ﻿using System;
+
 namespace string_calculator_kata {
     public class StringCalculator {
         public StringCalculator() {
         }
 
-        public int Calculate(String input) {
+        public int Calculate(String input)
+        {
+
+            string[] stringNumbers;
             if (input == "")
                 return 0;
+            //split the input 
+            else if (input.Contains(','))
+            {
+               stringNumbers = input.Split(',');
+               int sum = 0;
+               foreach (string stringNum in stringNumbers)
+               {
+                   sum += Int32.Parse(stringNum);
+                 
+               }
+
+               return sum;
+            }
             else
                 return Int32.Parse(input);
+            
+            
+            
+            
         }
+        
     }
 }
