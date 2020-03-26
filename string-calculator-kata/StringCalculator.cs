0af166df782d03420b363fe8ae5d54432642ad8c
Michelle <wong.mich.c@gmail.com>
2020-03-26 15:54:35 +1100

    started code to pass test 6

--- string-calculator-kata/StringCalculator.cs
+++ string-calculator-kata/StringCalculator.cs
@@ -9,26 +9,32 @@ namespace string_calculator_kata {
         {
 
             string[] stringNumbers;
+            string delimiter = "";
             if (input == "")
                 return 0;
             //split the input 
-            else if (input.Contains(',') || input.Contains('\n'))
+            else if (input.Contains("//"))
             {
+                delimiter = input[2].ToString();
+                stringNumbers = input.Split(delimiter);
 
-               stringNumbers = input.Split(',', '\n');
+            }
 
-               int sum = 0;
-               foreach (string stringNum in stringNumbers)
-               {
-                   sum += Int32.Parse(stringNum); 
-               }
+            else if ()
+            {
+                int sum = 0;
+                foreach (string stringNum in stringNumbers)
+                {
+                    sum += Int32.Parse(stringNum);
+                }
 
-               return sum;
+                return sum;
             }
             else
                 return Int32.Parse(input);
-           
+
         }
+    }
         
     }
 }
